0c1340a26d89caa78ac0aa6619c532117a7ad645
David Bebb <davidbebb@gmail.com>
2016-04-17 20:58:12 +0100

    manual around curve

--- hot-end-mount.py
+++ hot-end-mount.py
@@ -24,29 +24,46 @@ plane.add_layer(
 
 x = width/2
 y = height/4
-crush=.75
+crush=0.1
 pos = V(0,0)
 bolt_offset = 10
 main_bolt_offset = 30.7/2
-hotend_rad=6
+hotend_rad=6.1
+step_rad=8
+step_depth=0.75
+x_offset=hotend_rad+(cutter_rad*2)
 
 border = Path(closed=True, side='out')
-border.add_point(PIncurve(pos + V( x,  y), radius=0, direction = 'CW'))
-border.add_point(PIncurve(pos + V( x, -y), radius=0, direction = 'CW'))
+border.add_point(PIncurve(pos + V(-x, -y), radius=corner_rad, direction = 'CW'))
+border.add_point(PIncurve(pos + V(-x,  y), radius=corner_rad, direction = 'CW'))
 
-border.add_point(PAroundcurve(pos + V(0, -y), centre=pos+V(main_bolt_offset,-(y+crush/2)), radius=0.5, direction = 'CW'))
-border.add_point(PAroundcurve(pos + V(0, -y), centre=pos+V(bolt_offset,-(y+crush/2)), radius=0.5, direction = 'CW'))
-border.add_point(PAroundcurve(pos + V(0, -y), centre=pos+V(0,-(y+crush/2)), radius=hotend_rad, direction = 'CW'))
-border.add_point(PAroundcurve(pos + V(0, -y), centre=pos+V(-bolt_offset,-(y+crush/2)), radius=0.5, direction = 'CW'))
-border.add_point(PAroundcurve(pos + V(0, -y), centre=pos+V(-main_bolt_offset,-(y+crush/2)), radius=0.5, direction = 'CW'))
+border.add_point(PIncurve(pos + V( -x_offset,  y), direction = 'CW'))
+border.add_point(POutcurve(pos + V(0,y+0.1), radius=hotend_rad, direction = 'CW'))
+border.add_point(PIncurve(pos + V(  x_offset,  y), direction = 'CW'))
 
-border.add_point(PIncurve(pos + V(-x, -y), radius=0, direction = 'CW'))
-border.add_point(PIncurve(pos + V(-x,  y), radius=0, direction = 'CW'))
+border.add_point(PIncurve(pos + V( x,  y), radius=corner_rad, direction = 'CW'))
+border.add_point(PIncurve(pos + V( x, -y), radius=corner_rad, direction = 'CW'))
 
-border.add_point(PAroundcurve(pos + V(0, y), centre=pos+V(-main_bolt_offset,(y+crush/2)), radius=0.5, direction = 'CW'))
-border.add_point(PAroundcurve(pos + V(0, y), centre=pos+V(-bolt_offset,(y+crush/2)), radius=0.5, direction = 'CW'))
-border.add_point(PAroundcurve(pos + V(0, y), centre=pos+V(bolt_offset,(y+crush/2)), radius=0.5, direction = 'CW'))
-border.add_point(PAroundcurve(pos + V(0, y), centre=pos+V(main_bolt_offset,(y+crush/2)), radius=0.5, direction = 'CW'))
+
+# border = Path(closed=True, side='out')
+# border.add_point(PIncurve(pos + V( x,  y), radius=0, direction = 'CW'))
+# border.add_point(PIncurve(pos + V( x, -y), radius=0, direction = 'CW'))
+#
+# border.add_point(PAroundcurve(pos + V(0, -y), centre=pos+V(main_bolt_offset,-(y+crush/2)), radius=0.5, direction = 'CW'))
+# border.add_point(PAroundcurve(pos + V(0, -y), centre=pos+V(bolt_offset,-(y+crush/2)), radius=0.5, direction = 'CW'))
+#
+# border.add_point(PAroundcurve(pos + V(0, -y), centre=pos+V(0,-(y+2)), radius=hotend_rad, direction = 'CW'))
+#
+# border.add_point(PAroundcurve(pos + V(0, -y), centre=pos+V(-bolt_offset,-(y+crush/2)), radius=0.5, direction = 'CW'))
+# border.add_point(PAroundcurve(pos + V(0, -y), centre=pos+V(-main_bolt_offset,-(y+crush/2)), radius=0.5, direction = 'CW'))
+#
+# border.add_point(PIncurve(pos + V(-x, -y), radius=0, direction = 'CW'))
+# border.add_point(PIncurve(pos + V(-x,  y), radius=0, direction = 'CW'))
+#
+# border.add_point(PAroundcurve(pos + V(0, y), centre=pos+V(-main_bolt_offset,(y+crush/2)), radius=0.5, direction = 'CW'))
+# border.add_point(PAroundcurve(pos + V(0, y), centre=pos+V(-bolt_offset,(y+crush/2)), radius=0.5, direction = 'CW'))
+# border.add_point(PAroundcurve(pos + V(0, y), centre=pos+V(bolt_offset,(y+crush/2)), radius=0.5, direction = 'CW'))
+# border.add_point(PAroundcurve(pos + V(0, y), centre=pos+V(main_bolt_offset,(y+crush/2)), radius=0.5, direction = 'CW'))
 
 mount_1 = plane.add(
     Part(name = 'mount-1',
